92e1052f6930b69214d19ea93d09717999649842
guidev279 <guidev279@gmail.com>
2025-10-01 15:03:52 -0300

    adciona project

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const btnSearch = document.getElementById('btn-search')
+const inputSearch = document.getElementById('input-search')
+const profileResults = document.getElementById('profile-results')
+
+const baseUrl = 'https://api.github.com';
+
+ btnSearch.addEventListener('click', async () => {
+    const userName = inputSearch.value;
+    if(userName){
+
+        try{
+            const response = await fetch(`${baseUrl}/users/${userName}`)
+        
+            if(!response.ok){
+                alert('Usuário não encontado. Por favor, verifique o nome do usuário digitado e tente novamente.')
+            }
+
+            const user = await response.json()
+
+            profileResults.innerHTML = `
+                <div class="profile-card">
+                    <img src="${user.avatar_url}" alt="Avatar de ${user.name}" class="profile-avatar">
+                    <div class="profile-info">
+                        <h2>${user.name}</h2>
+                        <p>${user.bio || 'Não possui bio cadastrada 😢 '}</p>
+                    </div>
+                </div>
+            `
+
+    }catch (error){
+        console.error('Erro ao buscar perfil do usuário', error);
+        alert('Ocorreu um erro ao buscar perfil do usuário. Por favor, tente novamente mais tarde.')
+    }
+    }else{
+        alert('Por favor, digite um nome de usuário do GitHub')
+    };
+});
