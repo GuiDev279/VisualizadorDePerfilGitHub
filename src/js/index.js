fe81be061f8ada953454511559364f43ab9bc553
guidev279 <guidev279@gmail.com>
2025-10-02 09:55:28 -0300

    adicionado os repositorios

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,22 @@ const profileResults = document.getElementById('profile-results')
 
 const baseUrl = 'https://api.github.com';
 
- btnSearch.addEventListener('click', async () => {
-    const userName = inputSearch.value;
-    if(userName){
 
-        try{
+btnSearch.addEventListener('click', async () => {
+    const userName = inputSearch.value;
+
+    if (userName) {
+
+        try {
+
             const response = await fetch(`${baseUrl}/users/${userName}`)
-        
-            if(!response.ok){
+
+            if (!response.ok) {
                 alert('Usuário não encontado. Por favor, verifique o nome do usuário digitado e tente novamente.')
             }
 
             const user = await response.json()
+
 
             profileResults.innerHTML = `
                 <div class="profile-card">
@@ -36,13 +40,51 @@ const baseUrl = 'https://api.github.com';
                         <span>${user.following}</span>
                     </div>
                 </div>
-            `
+                <hr>
+            `;
 
-    }catch (error){
-        console.error('Erro ao buscar perfil do usuário', error);
-        alert('Ocorreu um erro ao buscar perfil do usuário. Por favor, tente novamente mais tarde.')
-    }
-    }else{
+            // Buscar repositórios (últimos 10 criados)
+            const reposResponse = await fetch(`${baseUrl}/users/${userName}/repos?per_page=10&sort=created`);
+
+            if (!reposResponse.ok) {
+                throw new Error('Não foi possível buscar os repositórios.');
+            }
+
+            const repos = await reposResponse.json();
+
+
+            // Renderizar lista de repositórios
+            const reposHtml = repos.length > 0 ? repos.map(repo => `
+
+                <a href="${repo.html_url}" target="_blank">
+                    <div class="repository-card">
+                            <h4>${repo.name}</h4>
+                            <div class="repository-stats">
+                                <span>⭐ Stars: ${repo.stargazers_count}</span>
+                                <span>🍴 Forks: ${repo.forks_count}</span>
+                                <span>👀 Watchers: ${repo.watchers_count}</span>
+                                <span>💻 Language: ${repo.language || 'Não informado'}</span>
+                            </div>
+                    </div>
+                </a>
+            `).join('') : `<p>Este usuário não possui repositorios públicos</p>`;
+
+
+            // Aqui você insere no HTML
+            profileResults.innerHTML += `
+                    <div class="profile-repositories">
+                        <h3>📂 Repositórios</h3>
+                        <div class="repositories">
+                            ${reposHtml}
+                        </div>
+                    </div>
+                `;
+
+        } catch (error) {
+            console.error('Erro ao buscar perfil do usuário', error);
+            alert('Ocorreu um erro ao buscar perfil do usuário. Por favor, tente novamente mais tarde.')
+        }
+    } else {
         alert('Por favor, digite um nome de usuário do GitHub')
     };
 });
